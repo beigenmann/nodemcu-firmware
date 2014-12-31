8c5f05111662e4e294cd5523174b35c1bf881b18
Benno Eigenmann <beigenmann@gmail.com>
2014-12-31 07:39:39 +0100

    inti

---
+++ lua_examples/init.lua
@@ -0,0 +1,7 @@
+print('init.lua ver 1.2')
+print('set mode=STATION (mode='..wifi.getmode()..')')
+print('MAC: ',wifi.sta.getmac())
+print('chip: ',node.chipid())
+print('heap: ',node.heap())
+dofile('telnet.lua')
+
